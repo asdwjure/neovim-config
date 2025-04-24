89708948f59af03e36562749b48732bcad0b4819
Jure Rebernik <jure.rebernik@luxonis.com>
2025-04-24 15:31:32 +0200

    improve treesitter behaviour

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,52 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	-- update all installed parsers synchronously after install
-	build = function()
-		require('nvim-treesitter.install').update { with_sync = true }
-	end,
-	config = function()
-		require('nvim-treesitter.configs').setup {
-			ensure_installed = {
-				'c',
-				'cpp',
-				'python',
-				'lua',
-				'vim',
-				'vimdoc',
-				'yaml',
-				'markdown',
-				'markdown_inline',
-			},
-			sync_install = false,
-			highlight = { enable = true },
-			indent    = { enable = true },
-		}
-	end,
+  'nvim-treesitter/nvim-treesitter',
+  build = function()
+    require('nvim-treesitter.install').update { with_sync = true }
+  end,
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = {
+        'c','lua','vim','vimdoc','query','elixir','heex','javascript','html',
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent    = { enable = true },
+
+      textobjects = {
+        select = {
+          enable    = true,     -- load the `locals` queries so @local.scope works if you ever need it
+          lookahead = true,
+          keymaps   = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select scope" },
+          },
+        },
+
+        move = {
+          enable    = true,
+          set_jumps = true,     -- populate jumplist so <C-o> works
+
+          goto_next_start = {
+            ["]f"] = "@function.outer",  -- next function start
+            ["]s"] = "@block.outer",     -- next block ({…}) start
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",  -- prev function start
+            ["[s"] = "@block.outer",     -- prev block start
+          },
+
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+          },
+        },
+      },
+    }
+  end,
 }
