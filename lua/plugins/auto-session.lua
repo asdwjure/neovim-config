f2e7c16a93473679dac9af85f5fdef458749a4f1
Jure Rebernik <jure.rebernik@luxonis.com>
2025-04-26 15:38:59 +0200

    load auto-session very early and not lazy
    There were some issues with auto-session not loading the session
    properly at startup. By loading it very early, this was avoided.

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,31 +1,33 @@
 return {
-  'rmagatti/auto-session',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  config = function()
-    require('auto-session').setup({
-      -- where all session files live
-      root_dir                 = vim.fn.stdpath('data') .. '/sessions/',
+    'rmagatti/auto-session',
+    lazy = false,    -- <-- NOT lazy loaded!
+    priority = 1000, -- <-- VERY early
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
+    config = function()
+        require('auto-session').setup({
+            -- where all session files live
+            root_dir                 = vim.fn.stdpath('data') .. '/sessions/',
 
-      -- auto-create a session file for *this* project (cwd) on exit
-      auto_create              = true,
+            -- auto-create a session file for *this* project (cwd) on exit
+            auto_create              = true,
 
-      -- auto-save it when you exit nvim
-      auto_save                = true,
+            -- auto-save it when you exit nvim
+            auto_save                = true,
 
-      -- auto-restore it on startup *only* if there's a session for this cwd
-      auto_restore             = true,
+            -- auto-restore it on startup *only* if there's a session for this cwd
+            auto_restore             = true,
 
-      -- do *not* fall back to the “last” session when starting in a new dir
-      auto_restore_last_session = false,
+            -- do *not* fall back to the “last” session when starting in a new dir
+            auto_restore_last_session = false,
 
-      -- name sessions *only* by their project path (not by git-branch)
-      git_use_branch_name      = false,
+            -- name sessions *only* by their project path (not by git-branch)
+            git_use_branch_name      = false,
 
-      -- keep auto-session from kicking in under your home or root dir
-      suppressed_dirs          = { '~/', '/' },
-    })
-  end,
+            -- keep auto-session from kicking in under your home or root dir
+            suppressed_dirs          = { '~/', '/' },
+        })
+    end,
 }
 
