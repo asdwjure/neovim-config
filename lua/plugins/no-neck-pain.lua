c295111d1067bd6432a20c7dc8c003853acedf7b
Jure Rebernik <jure.rebernik@luxonis.com>
2025-04-26 15:39:32 +0200

    enable no-neck-pain at startup

--- lua/plugins/no-neck-pain.lua
+++ lua/plugins/no-neck-pain.lua
@@ -1,19 +1,24 @@
 return {
-  'shortcuts/no-neck-pain.nvim',
-  version = '*',    -- use latest tag
-  config = function()
-    require('no-neck-pain').setup({
-      -- desired width of the focused buffer;
-      -- when your total window width is < this, it won’t create padding
-      width = 160,
+    'shortcuts/no-neck-pain.nvim',
+    version = '*',    -- use latest tag
+    event = "VimEnter",
+    priority = 1, -- very low priority
+    config = function()
+        require('no-neck-pain').setup({
+            -- desired width of the focused buffer;
+            -- when your total window width is < this, it won’t create padding
+            width = 160,
 
-      mappings = {
-        enabled = false, -- mappings defined in remap.lua
-      },
-      -- you can tweak these defaults if you like more/less padding:
-      minSideBufferWidth = 10,
-      killAllBuffersOnDisable = false,
-      fallbackOnBufferDelete = true,
-    })
-  end,
+            mappings = {
+                enabled = false, -- mappings defined in remap.lua
+            },
+            -- you can tweak these defaults if you like more/less padding:
+            minSideBufferWidth = 10,
+            killAllBuffersOnDisable = false,
+            fallbackOnBufferDelete = true,
+            autocmds = {
+                enableOnVimEnter = true,
+            }
+        })
+    end,
 }
