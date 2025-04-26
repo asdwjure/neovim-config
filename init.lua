862ff6cd10d7cac0fe86c607fb25c4b4a0b77a33
Jure Rebernik <jure.rebernik@luxonis.com>
2025-04-26 15:36:43 +0200

    print load time at startup

--- init.lua
+++ init.lua
@@ -4,3 +4,11 @@ require("config.set")
 require("config.lazy")
 require("config.remap")
 
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyVimStarted",
+  callback = function()
+    local stats = require("lazy").stats()
+    local ms = math.floor(stats.startuptime * 100) / 100
+    print("Neovim loaded in " .. ms .. "ms with " .. stats.count .. " plugins")
+  end,
+})
